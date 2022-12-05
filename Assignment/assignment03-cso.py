23be8d8d5d6518d2c183a41c170dbd881306f496
Ruth School <ruthschool@Ruths-MacBook-Air.local>
2022-12-05 16:26:32 +0000

    Assignment 3 update

--- Assignment/assignment03-cso.py
+++ Assignment/assignment03-cso.py
@@ -1,2 +1,15 @@
 #To do: Write a program that retrieves the dataset for the "exchequer account (historical series)" from the CSO, and stores it into a file called "cso.json"
 
+import requests
+import json
+url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/1.0/en"
+
+
+def getData():
+    response = requests.get(url)
+    return response.json()
+
+
+if __name__ == "__main__":
+    with open ("cso.json", "wt") as fp:
+        print(getData(),file = fp)
