59192065dbdfe32f0d7abc46af1c274fa2a7f2b8
Ruth School <ruthschool@Ruths-MacBook-Air.local>
2022-12-05 18:34:23 +0000

    Assignment 4  update

--- Assignment/assignment04.py
+++ Assignment/assignment04.py
@@ -1,3 +1,18 @@
 # Write a program in python that will read a file from a repository, 
 # The program should then replace all the instances of the text "Andrew" with your name
 # The program should then commit those changes and push the file back to the repository.
+
+
+import requests
+import json
+
+filename = "repo-public.json"
+url = "https://api.github.com/repos/keady-r/data-representation-coursework"
+
+response = requests.get(url)
+print(response.status_code)
+repoJSON = response.json()
+
+
+with open (filename, "w") as fp:
+    json.dumps(repoJSON, fp, indent=4)
